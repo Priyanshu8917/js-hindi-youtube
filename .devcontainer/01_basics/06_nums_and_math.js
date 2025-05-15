1118aa12b47f13d6a9a03b6371a9885492f37282
Priyanshu Sarangi <priyanshusadangi123@gmail.com>
2025-05-15 21:02:30 +0530

    Math and num

---
+++ .devcontainer/01_basics/06_nums_and_math.js
@@ -0,0 +1,64 @@
+const score = 400
+// console.log(score);
+
+
+const Balance = new Number(100)
+// console.log(Balance);
+
+
+
+// console.log(Balance.toString().length);
+// console.log(Balance.toFixed(2));
+
+// const otherNumber = 23.8966
+  const otherNumber = 123.8966
+//  const otherNumber = 1123.8966
+
+
+
+
+
+
+
+// console.log(otherNumber.toPrecision(4));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+// console.log(Math.abs(-4)); // in abs here -ve value will be same as positive value
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));// it give top value
+// console.log(Math.floor(4.9));// it give lowest value
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+
+console.log(Math.random()); // returns value between 0 and 1
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+// a trick for min and max find 
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
+
+
+
