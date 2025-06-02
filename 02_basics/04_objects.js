d3470b6581f592f4b9074528132ee63c3aeb50d1
Priyanshu Sarangi <priyanshusadangi123@gmail.com>
2025-06-02 10:27:53 +0530

    JSON talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -47,13 +47,42 @@ const users = [
     },
 ]   
  users[1].email
- console.log(tenderUser);
+ //console.log(tenderUser);
 
- console.log(Object.keys(tenderUser)); //keys means: Returns the names of enumerable string properties and methods of an objects.
- console.log(Object.values(tenderUser));// returns an array of values of the enumerable own properties of an objects.
- console.log(Object.entries(tenderUser));//returns of array of key/values of the enumerable own properties of an object.
+ //console.log(Object.keys(tenderUser)); //keys means: Returns the names of enumerable string properties and methods of an objects.
+ //console.log(Object.values(tenderUser));// returns an array of values of the enumerable own properties of an objects.
+ //console.log(Object.entries(tenderUser));//returns of array of key/values of the enumerable own properties of an object.
 
- console.log(tenderUser.hasOwnProperty("isLoggedIn"));
+ //console.log(tenderUser.hasOwnProperty("isLoggedIn"));
+
+//Object de-structure and JSON API intro
+ const course ={
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Priyanshu"
+ }
+
+// course.courseInstructor (define karna kiliya)
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
+
+//React example 
+//const navbar = ({company}) => {
+
+//}
+
+//navbar(company = "Priyanshu")
+
+//{} this is called JSON
+
+// {
+//     "name": "Priyanshu",
+//     "coursename": "js in hindi",
+//     "price": "free" 
+// } this is called API
+
  
   
  
